e6d8bd2752a42d60aadb0b53cf32253350746b28
Novianis <ovisandra1411@gmail.com>
2026-01-16 21:17:31 +0700

    Add files via upload

---
+++ Tugas.py
@@ -0,0 +1,58 @@
+print ("aaa")
+
+
+
+
+
+nama = "bbb"
+print(f"ccc {nama}")
+
+
+
+
+
+def tampilkan():
+    print("ddd")
+
+
+
+
+
+class Tugas:
+    def __init__(self):
+        self.satu="eee"
+            
+ttugas=Tugas()
+print(ttugas.satu)
+
+
+
+
+
+class gg:
+    def __init__(self):
+        self.dua="fff"
+            
+nyeh=gg()
+print(f"ggg {nyeh.dua}")
+
+
+
+
+def inputan():
+    input("hhh")
+
+
+
+
+
+def main():
+    tampilkan()
+    print(f"{inputan()}")
+
+if __name__ == "__main__":
+    main()
+
+
+
+#Subjek Email : Nama-Kelas-AccessModifierSiswa
