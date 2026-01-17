9576c711dc9869818f566edf453940b522467779
Novianis <ovisandra1411@gmail.com>
2026-01-17 22:11:27 +0700

    Add files via upload

---
+++ TipeData.py
@@ -0,0 +1,36 @@
+print('==================Tipe Data================')
+# integer
+a = 1 # Bilangan Bulat
+print('tipe data dari variable a adalah', type(a))
+
+# float
+b = 1.5 # Desimal
+print('tipe data dari variable b adalah', type(b))
+
+# string
+c = 'Asep' # Nama
+print('tipe data dari variable c adalah', type(c))
+
+# boolean
+d = True
+e = False
+print('tipe data dari variable d adalah', type(d))
+print('tipe data dari variable e adalah', type(e))
+
+# list
+f = [1, 2, 3] # Menyimpan tipe data dan isinya bisa diubah
+print('tipe data dari variable f adalah', type(f))
+
+# tuple
+g = (1, 2, 3) # Menyimpan tipe data dan isinya tidak bisa diubah
+print('tipe data dari variable g adalah', type(g))
+
+# dictionary
+h = {'nama': 'Asep', 'umur': 25, 'kelas': 'A'} # Menyimpan tipe data berupa pasangan penunjuk dan nilai
+print('tipe data dari variable h adalah', type(h))
+
+# set
+i = {1, 2, 3}
+print('tipe data dari variable i adalah', type(i))
+
+print('=================================================')
