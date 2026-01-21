59f7d95c0e8c74a2361f52a8d87d5b5bb78a36d6
Novianis <ovisandra1411@gmail.com>
2026-01-21 21:41:16 +0700

    Add files via upload

---
+++ classobjectkeluarga.py
@@ -0,0 +1,31 @@
+# Tugas instanceatribut
+
+class Keluarga:
+    def __init__(self):
+        self.nama=''
+        self.status=''
+        self.panggilan=''
+        self.umur=''
+print("===========================================")
+Orang1=Keluarga()
+Orang1.nama='Marwa'
+Orang1.status='Ibu'
+Orang1.panggilan='Marwa'
+Orang1.umur='64 Tahun'
+
+print(f'Nama : {Orang1.nama}')
+print(f'Status : {Orang1.status}')
+print(f'Panggilan : {Orang1.panggilan}')
+print(f'Umur : {Orang1.umur}')
+print("===========================================")
+Orang2=Keluarga()
+Orang2.nama='Nur Buat'
+Orang2.status='Kakak Laki-Laki'
+Orang2.panggilan='Nur'
+Orang2.umur='41 Tahun'
+
+print(f'Nama : {Orang2.nama}')
+print(f'Status : {Orang2.status}')
+print(f'Panggilan : {Orang2.panggilan}')
+print(f'Umur : {Orang2.umur}')
+print("===========================================")
