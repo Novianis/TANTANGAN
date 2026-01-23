2c57309a689b128c67e453a04afd7d75745cd600
Novianis <ovisandra1411@gmail.com>
2026-01-24 00:44:40 +0700

    Add files via upload

---
+++ instanceatribut1.py
@@ -0,0 +1,13 @@
+class Teman:
+    def __init__(self):
+        self.nama = "Nopi"
+        self.hobi = "maen"
+        self.kelas = "x RPL 2"
+        self.tahunlahir = "2008"
+
+Teman1 = Teman()
+
+print(f"Nama : {Teman1.nama}")
+print(f"Hobi : {Teman1.hobi}")
+print(f"Kelas : {Teman1.kelas}")
+print(f"Tahun Lahir : {Teman1.tahunlahir}")
