d4bee59e447234ce0ce8ffdcb559b374be61d417
Novianis <ovisandra1411@gmail.com>
2026-01-23 22:00:52 +0700

    Add files via upload

---
+++ CLASS.py
@@ -0,0 +1,28 @@
+class Teman:
+    def __init__(self):
+        self.nama1="Shyta"
+        self.hobi1="Main Game"
+        self.kelas1="X RPL 2"
+
+        self.nama2="Fira"
+        self.hobi2="Memasak"
+        self.kelas2="X RPL 2"
+        
+        self.nama3="Virda"
+        self.hobi3="Membaca"
+        self.kelas3="X RPL 2"
+
+TemanHebat=Teman()
+print(f"Nama : {TemanHebat.nama1}")
+print(f"Hobi : {TemanHebat.hobi1}")
+print(f"Kelas : {TemanHebat.kelas1}")
+
+TemanBaik=Teman()
+print(f"Nama: {TemanBaik.nama2}")
+print(f"Hobi : {TemanBaik.hobi2}")
+print(f"Kelas : {TemanBaik.kelas2}")
+
+TemanLucu=Teman()
+print(TemanLucu.nama3)
+print(TemanLucu.hobi3)
+print(TemanLucu.kelas3)
