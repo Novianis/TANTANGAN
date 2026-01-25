5e87d00d0672ba07ff679758e5e962e1fdaf6432
Novianis <ovisandra1411@gmail.com>
2026-01-25 20:26:20 +0700

    Add files via upload

---
+++ PyRumus.py
@@ -0,0 +1,9 @@
+import math
+def luas_persegi_panjang(panjang: float, lebar: float) -> float:
+    return panjang * lebar
+
+panjang = 15
+lebar = 7
+luas = luas_persegi_panjang(panjang, lebar)
+
+print(f"Luas Persegi Panjang: {luas} m3")
