d02344a3e292a76f2766e95ee708259d9df052e1
Novianis <ovisandra1411@gmail.com>
2026-01-26 14:40:03 +0700

    Add files via upload

---
+++ Kondisi.py
@@ -0,0 +1,66 @@
+angka1 = 2
+angka2 = 3
+
+# penjumlahan
+hasil = angka1 + angka2
+
+# pengurangan
+hasil = angka1 - angka2
+
+# pembagian
+hasil = angka1 / angka2
+
+# modulus
+hasil = angka1 % angka2
+
+# penjumlahan modulus
+hasil = (angka1 + angka2) % angka2
+
+
+
+
+a = 10
+b = 20
+c = a + b
+
+d = a + c
+e = b - d
+
+
+
+nilai = 76
+kkm = 75
+
+if nilai > 75:
+    print ("Good Job")
+else:
+    print ("Kamu Harus Remidi")
+
+
+
+
+# tugas
+
+saya = "5"
+standarditerima = "5"
+
+if saya == standarditerima:
+    print ("Saya diterima menjadi pacar :)")
+
+
+saya = "1"
+standarditerima = "5"
+
+if saya < standarditerima:
+    print ("Saya diterima menjadi pacar :)")
+else:
+    print ("Saya tidak diterima menjadi pacar TvT")
+
+
+isi = 5
+bensin = 5
+
+if isi == bensin:
+    print ("Bensin saya lebih dari 1 liter :D, jadi saya berangkaattt :)")
+else: 
+    print ("Bensin saya kurang dari 1 liter , membuat saya tidak bisa berangkat TvT")
