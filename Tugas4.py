4cd90721a597aedeff0b59e4396e2c17bbd2b24b
Novianis <ovisandra1411@gmail.com>
2026-02-09 17:34:02 +0700

    Add files via upload

--- Tugas4.py
+++ Tugas4.py
@@ -1,16 +1,6 @@
-import json
+# Dict dan List dalam Dict
 
-nama = input("Nama: ")
-email = input("Email: ")
-kelas1 = input("Kelas 1: ")
-kelas2 = input("Kelas 2: ")
-
-data = {
-    "siswa1": {"nama": nama, "email": email},
-    "kelas": [kelas1, kelas2]
+{
+    {""},
+    [""]
 }
-
-json.dump(data, open("Tugas4.json", "w"), indent=4)
-print("Data4 disimpan.")
-
-# Dict dan list dalam dict
